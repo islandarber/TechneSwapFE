954f190511bf213c8cb375e7111d74879b81c7af
Christina Vekri <christina.vek@gmail.com>
2024-03-12 11:02:57 +0100

    fixed some bugs

--- src/views/OtherUserProfil.jsx
+++ src/views/OtherUserProfil.jsx
@@ -43,17 +43,18 @@ export const OtherUserProfil = () => {
 
   return (
     <section className="bg-blueGray-50">
-      <div className="w-full lg:w-4/12 px-4 mx-auto mt-6"> {/* Adjusted mt-16 to mt-0 */}
-        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg">
+      <button onClick={() => window.history.back()} className="bg-custom-orange text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 ml-2 mt-4">Back</button>
+      <div className="w-full flex justify-center lg:w-4/12 px-4 mx-auto mt-6"> {/* Adjusted mt-16 to mt-0 */}
+      <div className="relative flex flex-col self-center min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg md:w-3/4 lg:w-1/2 xl:w-1/3">
           <div className="px-6 m-6">
             <div className="flex justify-center">
               <div className="relative">
                 {user.img && (
                   <img
-                    alt="Profile"
-                    src={user.img ? user.img : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"}
-                    className="shadow-xl rounded-md h-auto align-middle border-none max-w-150-px"
-                  />
+                  alt="Profile"
+                  src={user.img ? user.img : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"}
+                  className="shadow-xl rounded-md h-auto w-auto max-h-45 max-w-45 md:max-h-60 md:max-w-60 lg:max-h-80 lg:max-w-80 xl:max-h-96 xl:max-w-96"
+                />
                 )}
               </div>
             </div>
@@ -107,6 +108,8 @@ export const OtherUserProfil = () => {
             <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
               <div className="flex flex-wrap justify-center">
                 <div className="w-full lg:w-9/12 px-4">
+                  <h2 className='text-xl'>About me </h2>
+                  <hr className="mx-auto w-1/6 my-2 h-2 bg-blueGray-200" />
                   <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
                     {user.description || "No bio specified"}
                   </p>
